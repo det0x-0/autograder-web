06c03683a8b0b00430b7cd5d649e29d71c6f4025
Aashiq Rasool <aashiqrasool371@gmail.com>
2026-03-29 15:54:24 +0530

    Fix indentation to match proxy resubmit style and fix CI fail

--- site/js/modules/webui/courses/assignment/proxy/submit.js
+++ site/js/modules/webui/courses/assignment/proxy/submit.js
@@ -4,7 +4,7 @@ import * as Render from '../../../render/index.js';
 import * as Submit from '../submit.js';
 
 function init() {
-    Core.Routing.addRoute(Core.Routing.PATH_PROXY_SUBMIT, handlerProxySubmit, 'Assignment Proxy Submit', Core.Routing.NAV_COURSES, { assignment: true });
+    Core.Routing.addRoute(Core.Routing.PATH_PROXY_SUBMIT, handlerProxySubmit, 'Assignment Proxy Submit', Core.Routing.NAV_COURSES, {assignment: true});
 }
 
 function handlerProxySubmit(path, params, context, container) {
@@ -25,23 +25,25 @@ function handlerProxySubmit(path, params, context, container) {
             required: true,
             placeholder: 'Email',
         }),
-        new Render.FieldType(context, 'time', 'Proxy Time', {
-            type: Render.INPUT_TYPE_INT,
-        }),
         new Render.FieldType(context, 'message', 'Message', {
             type: Render.INPUT_TYPE_STRING,
+        }),
+        new Render.FieldType(context, 'time', 'Proxy Time', {
+            type: Render.INPUT_TYPE_INT,
         }),
     ];
 
     Render.makePage(
-        params, context, container, proxySubmit,
-        {
-            header: 'Proxy Submit',
-            description: 'Proxy submit an assignment submission to the autograder.',
-            inputs: inputFields,
-            buttonName: 'Submit',
-            iconName: Render.ICON_NAME_PROXY_SUBMIT,
-        });
+            params, context, container, proxySubmit,
+            {
+                header: 'Proxy Submit',
+                description: 'Proxy submit an assignment submission to the autograder.',
+                inputs: inputFields,
+                buttonName: 'Submit',
+                iconName: Render.ICON_NAME_PROXY_SUBMIT,
+            },
+        )
+    ;
 }
 
 function proxySubmit(params, context, container, inputParams) {
@@ -49,13 +51,13 @@ function proxySubmit(params, context, container, inputParams) {
     let assignment = course.assignments[params[Core.Routing.PARAM_ASSIGNMENT]];
 
     return Autograder.Courses.Assignments.Submissions.Proxy.submit(
-        course.id, assignment.id,
-        inputParams.files, inputParams.email, inputParams.time, inputParams.message,
-    )
-        .then(function (result) {
+            course.id, assignment.id,
+            inputParams.files, inputParams.email, inputParams.time, inputParams.message,
+        )
+        .then(function(result) {
             return Submit.getSubmissionResultHTML(course, assignment, result);
         })
-        .catch(function (message) {
+        .catch(function(message) {
             console.error(message);
             return message;
         });
--- site/js/modules/webui/courses/assignment/proxy/submit.test.js
+++ site/js/modules/webui/courses/assignment/proxy/submit.test.js
@@ -5,11 +5,11 @@ import * as Core from '../../../core/index.js';
 import * as Render from '../../../render/index.js';
 import * as Test from '../../../test/index.js';
 
-test('Proxy Submit, Success', async function () {
+test('Proxy Submit, Success', async function() {
     await Test.loginUser('course-admin');
     await Test.navigate(
-        Core.Routing.PATH_PROXY_SUBMIT,
-        { [Core.Routing.PARAM_COURSE]: 'course101', [Core.Routing.PARAM_ASSIGNMENT]: 'hw0' },
+            Core.Routing.PATH_PROXY_SUBMIT,
+            {[Core.Routing.PARAM_COURSE]: 'course101', [Core.Routing.PARAM_ASSIGNMENT]: 'hw0'},
     );
 
     Test.checkPageBasics('hw0', 'assignment proxy submit');
@@ -21,8 +21,6 @@ test('Proxy Submit, Success', async function () {
     const fileObj = new File([fileContent], 'hw0_solution.py');
 
     document.querySelector('.input-field[data-name="files"] input')[Render.TEST_FILES_KEY] = [fileObj];
-
-    
     document.querySelector('.input-field #email').value = 'course-student@test.edulinq.org';
 
     await Test.submitTemplate();
@@ -32,11 +30,11 @@ test('Proxy Submit, Success', async function () {
     expect(results).toContain('Score');
 });
 
-test('Proxy Submit, User Not Found', async function () {
+test('Proxy Submit, User Not Found', async function() {
     await Test.loginUser('course-admin');
     await Test.navigate(
-        Core.Routing.PATH_PROXY_SUBMIT,
-        { [Core.Routing.PARAM_COURSE]: 'course101', [Core.Routing.PARAM_ASSIGNMENT]: 'hw0' },
+            Core.Routing.PATH_PROXY_SUBMIT,
+            {[Core.Routing.PARAM_COURSE]: 'course101', [Core.Routing.PARAM_ASSIGNMENT]: 'hw0'},
     );
 
     Test.checkPageBasics('hw0', 'assignment proxy submit');
@@ -48,7 +46,6 @@ test('Proxy Submit, User Not Found', async function () {
     const fileObj = new File([fileContent], 'hw0_solution.py');
 
     document.querySelector('.input-field[data-name="files"] input')[Render.TEST_FILES_KEY] = [fileObj];
-
     document.querySelector('.input-field #email').value = 'zzz@test.edulinq.org';
 
     await Test.submitTemplate();
@@ -57,4 +54,3 @@ test('Proxy Submit, User Not Found', async function () {
     expect(results).toContain('Grading Failed');
     expect(results).toContain('User not found.');
 });
-
